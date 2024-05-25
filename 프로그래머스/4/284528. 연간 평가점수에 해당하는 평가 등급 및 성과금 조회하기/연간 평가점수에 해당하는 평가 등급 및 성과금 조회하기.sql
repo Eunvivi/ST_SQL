f74c592eb5204a368b107e9b7252e6c69c133ecb
Eunbee Cho <123444582+Eunvivi@users.noreply.github.com>
2024-05-25 22:35:26 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,18 @@
+WITH CTE AS (
+    SELECT EMP_NO,
+        (CASE
+            WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C' END) AS GRADE,
+        (CASE
+            WHEN AVG(SCORE) >= 96 THEN 0.2
+            WHEN AVG(SCORE) >= 90 THEN 0.15
+            WHEN AVG(SCORE) >= 80 THEN 0.1
+        ELSE 0 END) AS cal
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+
+SELECT CTE.EMP_NO, e.EMP_NAME, CTE.GRADE, CTE.cal*e.SAL AS BONUS
+FROM CTE LEFT JOIN HR_EMPLOYEES e ON CTE.EMP_NO=e.EMP_NO
